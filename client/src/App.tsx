cbe18e7a571fd244482450156f90cd7f0232d605
wmiq84 <michaelwang3489@gmail.com>
2024-10-08 17:17:59 -0700

    Configure routers

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,28 @@
-import { CssBaseline, ThemeProvider } from '@mui/material';
+import { CssBaseline, ThemeProvider, Box } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
 import { useMemo } from 'react';
 import { themeSettings } from './theme';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from '@/scenes/navbar';
 
 function App() {
-	const theme = useMemo(() => createTheme(themeSettings), [])
-	return <div className="app">
-		<ThemeProvider theme={theme}>
-			<CssBaseline />
-		</ThemeProvider>
-	</div>;
+	const theme = useMemo(() => createTheme(themeSettings), []);
+	return (
+		<div className="app">
+			<BrowserRouter>
+				<ThemeProvider theme={theme}>
+					{/* creates a constant starting point across browsers */}
+					<CssBaseline />
+					<Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+						<Routes>
+							<Route path="/" element={<div>dashboard page</div>} />
+							<Route path="/predictions" element={<div>predictions page</div>} />
+						</Routes>
+					</Box>
+				</ThemeProvider>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;

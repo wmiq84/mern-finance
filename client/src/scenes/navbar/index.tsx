ddc3f257e68b75c43bb6999613ee212f1673ffb1
wmiq84 <michaelwang3489@gmail.com>
2024-10-08 17:58:58 -0700

    Complete navbar

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import PixIcon from '@mui/icons-material/Pix';
 import { Box, Typography, useTheme } from '@mui/material';
 import FlexBetween from '@/components/FlexBetween';
 
@@ -11,9 +12,38 @@ const Navbar = (props: Props) => {
 	return (
 		<FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[300]}>
 			{/* Left side of navbar */}
-            <FlexBetween gap="0.75rem">
-                
-            </FlexBetween>
+			<FlexBetween gap="0.75rem">
+				<PixIcon sx={{ fontSize: '28px' }} />
+				<Typography variant="h4" fontSize="16px">
+					MERN Finance
+				</Typography>
+			</FlexBetween>
+			<FlexBetween gap="2rem">
+				<Box sx={{ '&:hover': { color: palette.primary[100] } }}>
+					<Link
+						to="/"
+						onClick={() => setSelected('dashboard')}
+						style={{
+							color: selected === 'dashboard' ? 'inherit' : palette.grey[700],
+							textDecoration: 'inherit',
+						}}
+					>
+						Dashboard
+					</Link>
+				</Box>
+				<Box sx={{ '&:hover': { color: palette.primary[100] } }}>
+					<Link
+						to="/predictions"
+						onClick={() => setSelected('predictions')}
+						style={{
+							color: selected === 'predictions' ? 'inherit' : palette.grey[700],
+							textDecoration: 'inherit',
+						}}
+					>
+						Predictions
+					</Link>
+				</Box>{' '}
+			</FlexBetween>
 		</FlexBetween>
 	);
 };

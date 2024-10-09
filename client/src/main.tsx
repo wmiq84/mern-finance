00d5d3293425d0e8d93a8961ded94beed6d46d71
wmiq84 <michaelwang3489@gmail.com>
2024-10-09 10:33:12 -0700

    Initialize server directories

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from '@/App';
 import '@/index.css';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { api, Api } from '@/state/api';
+
+export const store = configureStore({
+	reducer: { [api.reducerPath]: api.reducer },
+	middleware: (getDefault) => getDefault().concat(api.middleware),
+});
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>

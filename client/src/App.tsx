03c86fe0c8a822ba5c8d3dd903d218bc8895955a
wmiq84 <michaelwang3489@gmail.com>
2024-10-08 17:44:22 -0700

    Initialize navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,6 +14,7 @@ function App() {
 					{/* creates a constant starting point across browsers */}
 					<CssBaseline />
 					<Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+						<Navbar />
 						<Routes>
 							<Route path="/" element={<div>dashboard page</div>} />
 							<Route path="/predictions" element={<div>predictions page</div>} />
--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -1,15 +1,20 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Box, Typography, useTheme } from '@mui/material';
+import FlexBetween from '@/components/FlexBetween';
 
 type Props = {};
 
 const Navbar = (props: Props) => {
 	const { palette } = useTheme();
+	const [selected, setSelected] = useState('dashboard');
 	return (
-		<Box display="flex" justifyContent="space-between" alignItems="center">
-			Navbar
-		</Box>
+		<FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[300]}>
+			{/* Left side of navbar */}
+            <FlexBetween gap="0.75rem">
+                
+            </FlexBetween>
+		</FlexBetween>
 	);
 };
 
